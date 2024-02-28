760edaf8a28e01924917c3302e8e06242a367240
patrickkivuvo <patrickkivuvo@gmail.com>
2024-02-28 08:54:02 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:admin_panel/constants.dart';
+import 'package:admin_panel/controllers/MenuAppController.dart';
+import 'package:admin_panel/screens/main/main_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Admin Panel',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: bgColor,
+        textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme)
+            .apply(bodyColor: Colors.white),
+        canvasColor: secondaryColor,
+      ),
+      home: MultiProvider(
+        providers: [
+          ChangeNotifierProvider(
+            create: (context) => MenuAppController(),
+          ),
+        ],
+        child: MainScreen(),
+      ),
+    );
+  }
+}
